7b889c4c936013a42b85d9dfb192811d72c80bee
esdrasdev91 <esdras.dev@hotmail.com>
2024-09-11 09:07:32 -0300

    create kafkaEvent

---
+++ src/main/java/org/br/mineradora/kafka/KafkaEvent.java
@@ -0,0 +1,10 @@
+package org.br.mineradora.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaEvent {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvent.class);
+
+}
