7c339fb9d272f5c4df1405d514a93c470450c5ac
esdrasdev91 <esdras.dev@hotmail.com>
2024-09-18 18:56:31 -0300

    update method generateOpportunityData

--- src/main/java/org/br/mineradora/service/OpportunityServiceImpl.java
+++ src/main/java/org/br/mineradora/service/OpportunityServiceImpl.java
@@ -55,7 +55,20 @@ public class OpportunityServiceImpl  implements OpportunityService{
 
     @Override
     public List<OpportunityDTO> generateOpportunityData() {
-        return List.of();
+
+        List<OpportunityDTO> opportunities = new ArrayList<>();
+        opportunityRepository.findAll()
+                .stream()
+                .forEach(item -> opportunities.add(
+                                OpportunityDTO.builder()
+                                .proposalId(item.getProposalId())
+                                .customer(item.getCustomer())
+                                .priceTonne(item.getPriceTonne())
+                                .lastDollarQuotation(item.getLastDollarQuotation())
+                                .build()
+                ));
+
+        return opportunities;
     }
 
 }
